a27d7428563122f362f79fc26ab1f6ae840fc353
Mark Prykhoda <hroscko@gmail.com>
2020-05-08 11:14:01 +0300

    E2E Tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,41 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('conFusion app is running!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+    let navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    const newSubmmit = page.getElement('button[type=submit]');
+    newSubmmit.click();
+
+    browser.pause();
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry)
+    );
   });
 });
